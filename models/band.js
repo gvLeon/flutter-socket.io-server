0b02cc188ecd14342f5de1162d22fd3a751d601f
Leonardo Garibaldi <gv_leonardo@outlook.com>
2021-05-09 21:09:26 -0600

    cambios sobre bandas terminado

---
+++ models/band.js
@@ -0,0 +1,15 @@
+//Modelo para cada banda creada
+
+const { v4: uuidV4 } = require('uuid');
+ 
+class Band {
+
+    constructor(name = 'no-name') {
+        this.id = uuidV4();
+        this.name = name;
+        this.votes = 0;
+    }
+
+}
+
+module.exports = Band;
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,22 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band('Queen') );
+bands.addBand( new Band('Bon Jovi') );
+bands.addBand( new Band('San benito') );
+bands.addBand( new Band('Metallica') );
+
 
 //Mensajes de sockets
 io.on('connection', client => {
 
     console.log('Cliente conectado');
+
+    // Emite al cliente que se acaba de conectar
+    client.emit('active-bands', bands.getBands() );
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -14,4 +27,29 @@ io.on('connection', client => {
 
         io.emit('mensaje', {admin: 'nuevo mensaje'});
     });
+
+
+    client.on('emitir-mensaje',(payload) => {
+        // EMITE A TODOS LOS CLIENTES CONECTADOS
+        // io.emit('nuevo-mensaje', payload);
+        
+        // EMITE A TODOS MENOS AL QUE LO EMITIO
+        client.broadcast.emit('nuevo-mensaje', payload);
+    } );
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands() );
+    });
+
 });
